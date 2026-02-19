0161adb474b037fa47036bc48c501b3c989f4214
Lokkesh <39264572+lokkeshjaya@users.noreply.github.com>
2026-02-19 12:54:24 +0530

    chore: apply security fixes terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,9 +17,10 @@ resource "aws_s3_bucket" "app_bucket" {
   acl    = "public-read"                        # Issue 1: public-read ACL
 }
 
+# Fixed IAM policy to follow least privilege principle
 resource "aws_iam_policy" "app_policy" {
-  name        = "app-full-access"
-  description = "Policy used by instances"
+  name        = "app-restricted-access"
+  description = "Policy with restricted permissions following least privilege"
 
   policy = <<EOF
 {
@@ -27,12 +28,12 @@ resource "aws_iam_policy" "app_policy" {
   "Statement": [
     {
       "Effect": "Allow",
-      "Action": [                               # Fixed: Replaced wildcard with specific required actions
+      "Action": [
         "s3:GetObject",
         "s3:PutObject",
         "s3:ListBucket"
       ],
-      "Resource": [                             # Fixed: Specified exact resource ARNs
+      "Resource": [
         "arn:aws:s3:::sample-app-terraform-bucket-12345",
         "arn:aws:s3:::sample-app-terraform-bucket-12345/*"
       ]
